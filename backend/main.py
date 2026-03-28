b6b1205038a54651612928d0bd4b7cecc26f4aa0
Suppa Hacka <146290696+SuppaHackaD@users.noreply.github.com>
2026-03-28 13:40:05 -0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -249,4 +249,14 @@ async def websocket_endpoint(websocket: WebSocket):
             del manager.muted_until[websocket]
     except json.JSONDecodeError:
         print("Recebeu algo que não é JSON. Ignorando...")
+
+#      ______ _   _____             _  __                                  
+#     |  ____| | |  __ \           | |/ /                                  
+#     | |__  | | | |__) |__ _   _  | ' / ___  _ __   __ _ _ __ ___   ___   
+#     |  __| | | |  ___/ __| | | | |  < / _ \| '_ \ / _` | '__/ _ \ / _ \  
+#     | |____| | | |   \__ \ |_| | | . \ (_) | | | | (_| | | | (_) | (_) | 
+#     |______|_| |_|   |___/\__, | |_|\_\___/|_| |_|\__, |_|  \___/ \___(_)
+#                            __/ |                   __/ |                 
+#                           |___/                   |___/                  
+#by SuppaHackaD
         
